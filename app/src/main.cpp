bcd20903e556d92a4b08505a578109196507011a
akowalew <ram.techen@gmail.com>
2019-12-08 19:37:08 +0100

    app/main: Add usage of filtering using low-pass kernel

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -8,19 +8,46 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <cstdio>
+#include <cstdlib>
+
+#include <system_error>
 
 #include "debug.hpp"
 #include "reader.hpp"
 #include "writer.hpp"
+#include "kernel.hpp"
+#include "filter_seq.hpp"
 
-void filter_image(const char* input_path, const char* output_path)
+int parse_kernel_size(const char* str)
 {
-    const auto src_image = read_image(input_path);
+    int kernel_size = std::stoi(str);
+    if(kernel_size <= 0)
+    {
+        throw std::system_error(EINVAL, std::generic_category(),
+            "Kernel size must be greater than zero");
+    }
+    else if((kernel_size % 2) != 1)
+    {
+        throw std::system_error(EINVAL, std::generic_category(),
+            "Kernel size must be an odd number");
+    }
 
-    // Dummy filter - noop filter
-    const auto dst_image = src_image;
+    return kernel_size;
+}
 
-    write_image(output_path, dst_image);
+void filter_image(const char* input_path, const char* output_path, int kernel_size)
+{
+    const auto src = read_image(input_path);
+    show_image("Source image", src);
+
+    auto kernel = Image32F(kernel_size, kernel_size, CV_32F);
+    low_pass_kernel(kernel);
+
+    auto dst = ImageU8(src.rows, src.cols, src.type());
+    filter2d_8_seq(src, dst, kernel);
+
+    write_image(output_path, dst);
+    show_image("Destination image", dst);
 }
 
 /**
@@ -36,18 +63,19 @@ void filter_image(const char* input_path, const char* output_path)
  */
 int main(int argc, char** argv)
 {
-    if(argc < 3)
+    if(argc < 4)
     {
-        printf("Usage: ./filter-image <input_path> <output_path>\n");
+        printf("Usage: ./filter-image <input_path> <output_path> <kernel_size>\n");
         return 1;
     }
 
-    const auto input_path = argv[1];
-    const auto output_path = argv[2];
-
     try
     {
-        filter_image(input_path, output_path);
+        const auto input_path = argv[1];
+        const auto output_path = argv[2];
+        const auto kernel_size = parse_kernel_size(argv[3]);
+
+        filter_image(input_path, output_path, kernel_size);
 
         wait_for_exit();
     }
